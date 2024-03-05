9940235e88fd00c7901a5b69cc6bb883408f40c2
Ashot <ashotd03@gmail.com>
2024-03-05 20:28:24 +0400

    working on parsing of the config file

--- config.cpp
+++ config.cpp
@@ -1,29 +1,28 @@
 #include "config.hpp"
-#include <sstream>
 
-void configFile::parseError(const parseException &ex) const{
+void ConfigFile::reportError(const ParseException &ex) const{
 	throw (ex);
 }
 
-void configFile::openConfig(){
+void ConfigFile::openConfig(){
 	this->config.open("test.conf");
 	if (!config.is_open()){
-		parseError(configFileException("Could not open the config file"));
+		reportError(configFileException("Could not open the config file"));
 	}
 }
 
-size_t configFile::skipWhitespace(const std::string &line, size_t pos) const{
+size_t ConfigFile::skipWhitespace(const std::string &line, size_t pos) const{
 	while (pos < line.size() && (line[pos] == ' ' || line[pos] == '	' || line[pos] == '\n'))
 		pos++;
 	return (pos);
 }
 
-size_t configFile::findPair(const std::string &line, size_t pos) const{
+size_t ConfigFile::findPair(const std::string &line, size_t pos) const{
 	if (pos >= line.size())
-		parseError(configFileException("Invalid configuration"));
+		reportError(configFileException("Invalid configuration"));
 	while (line[pos] != '}'){
 		if (pos >= line.size()){
-			parseError(configFileException("Invalid configuration"));
+			reportError(configFileException("Invalid configuration"));
 		}
 		if (line[pos] == '{'){
 			pos = findPair(line, pos + 1);
@@ -33,7 +32,7 @@ size_t configFile::findPair(const std::string &line, size_t pos) const{
 	return (pos);
 }
 
-void configFile::divideIntoServers(const std::string &line) {
+void ConfigFile::divideIntoServers(const std::string &line) {
 	size_t pos1 = 0;
 	size_t pos2 = 0;
 
@@ -45,7 +44,7 @@ void configFile::divideIntoServers(const std::string &line) {
 			serversTxt.push_back(line.substr(pos1, pos2 - pos1));
 			pos1 = skipWhitespace(line, pos2 + 1);
 		} else
-			parseError(configFileException("Invalid configuration"));
+			reportError(configFileException("Invalid configuration"));
 	}
 	
 	for (size_t i = 0; i < serversTxt.size(); i++){
@@ -53,7 +52,7 @@ void configFile::divideIntoServers(const std::string &line) {
 	}
 }
 
-void configFile::removeComments(std::string &line) {
+void ConfigFile::removeComments(std::string &line) {
 	size_t pos;
 	std::string line1;
 	std::string line2;
@@ -63,18 +62,78 @@ void configFile::removeComments(std::string &line) {
 		line2 = line.substr(line.find('\n', pos));
 		line = line1 + line2;
 	}
-	std::cout << line << std::endl;
 }
 
-void configFile::parseServers(){
-	if (serversTxt.size() < 1)
-		parseError(configException("No server configuration in the config file"));
-	for (size_t i = 0; i < serversTxt.size(); i++){
-		
+void ServerBlock::setServerName(std::string &line){
+	
+}
+void ServerBlock::addLocation(std::string &line){
+
+}
+void ServerBlock::addErrorPage(std::string &line){
+
+}
+void ServerBlock::setClientMaxBodySize(std::string &line){
+
+}
+void ServerBlock::setAutoindex(std::string &line){
+
+}
+void ServerBlock::setListen(std::string &line){
+
+}
+void ServerBlock::setPort(std::string &line){
+
+}
+
+void ServerBlock::addLocationTxt(const std::string &str){
+	locationsTxt.push_back(str);
+}
+
+void ConfigFile::cutLocations(){
+	size_t pos1 = 0;
+	size_t pos2 = 0;
+
+	for(size_t i = 0; i < serversTxt[i].size(); i++){
+		ServerBlock server;
+		while ((pos1 = serversTxt[i].find("location", pos1)) != std::string::npos){
+			pos1 += 8;
+			pos1 = skipWhitespace(serversTxt[i], pos1);
+			if (pos1 < serversTxt[i].size() && serversTxt[i][pos1] == '/')
+				pos1 = serversTxt[i].find(' ', pos1);
+			pos1 = skipWhitespace(serversTxt[i], pos1);
+			if (pos1 < serversTxt[i].size() && serversTxt[i][pos1] == '{'){
+				pos2 = findPair(serversTxt[i], ++pos1);
+				servers.addLocationTxt(serversTxt[i].substr(pos1, pos2 - pos1));
+				pos1 = skipWhitespace(serversTxt[i], pos2 + 1);
+			} else
+				reportError(configFileException("Invalid configuration"));
+		}
+		servers.push_back(server);
 	}
 }
 
-void configFile::parse(){
+void ConfigFile::parseServers(){
+	cutLocations();
+	std::string line;
+	std::string key;
+	std::map<std::string, void(*)(std::string)> keywords;
+	if (serversTxt.size() < 1)
+		reportError(configException("No server configuration in the config file"));
+
+	for (size_t i = 0; i < serversTxt.size(); i++){
+		std::stringstream ss(serversTxt[i]);
+		while (getline(ss, line, ';')){
+			size_t pos = skipWhitespace(line, 0);
+			key = line.substr(pos , line.find(' ', pos) - pos);
+			keywords[key](line.substr(line.find(' ', pos)));
+		}
+	}
+}
+
+
+
+void ConfigFile::parse(){
 	std::string line;
 
 	getline(config, line, '\0');
--- config.hpp
+++ config.hpp
@@ -3,69 +3,88 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <sstream>
+#include <map>
 
-class parseException : public std::exception {
+class ParseException : public std::exception {
 public:
-	parseException(const std::string& message) : _message(message) {}
+	ParseException(const std::string& message) : _message(message) {}
 	virtual const char* what() const throw() {
 		return _message.c_str();
 	}
-	virtual ~parseException() throw() {}
+	virtual ~ParseException() throw() {}
 private:
 	const std::string _message;
 };
 
-class configFileException : public parseException {
+class configFileException : public ParseException {
 public:
-	configFileException(const std::string &message) : parseException(message){}
+	configFileException(const std::string &message) : ParseException(message){}
 };
 
-class configException : public parseException {
+class configException : public ParseException {
 public:
-	configException(const std::string &message) : parseException(message){}
+	configException(const std::string &message) : ParseException(message){}
 };
 
-class IconfigParse{
+class IConfigParse{
 public:
 	virtual	void parse() = 0;
-	virtual void parseError(const parseException &ex) const = 0;
+	virtual void reportError(const ParseException &ex) const = 0;
 	virtual void openConfig() = 0;
 	virtual void divideIntoServers(const std::string &line) = 0;
 	virtual void parseServers() = 0;
 	virtual void removeComments(std::string &line) = 0;
-
+	virtual void cutLocations() = 0;
 	virtual size_t skipWhitespace(const std::string &line, size_t pos) const = 0;
 	virtual size_t findPair(const std::string &line, size_t pos) const = 0;
 };
 
-class locationBlock{
+class LocationBlock{
 private:
 
 };
 
-class serverBlock{
+//serverblock
+class ServerBlock{
 private:
-	std::string server_name;
-	std::vector<locationBlock> locations;
-	
+	std::string listen;
+	std::vector<std::string> server_names;
+	std::map<unsigned short, std::string> error_page;
+	unsigned int client_max_body_size;
+	bool autoindex;
+	short port;
+	std::vector<LocationBlock> locations;
+	std::vector<std::string> locationsTxt;
 public:
-	void setServer_name();
+	void setServerName();
 	void addLocation();
+	void addErrorPage();
+	void setClientMaxBodySize();
+	void setAutoindex();
+	void setListen();
+	void setPort();
+	void addLocationTxt(const std::string &str);
+	std::vector<std::string> &getServerNames() const;
+	std::vector<LocationBlock> &getLocations() const;
+	std::map<short, std::string> getErrorPage() const;
 };
 
-class configFile : public IconfigParse {
+class ConfigFile : public IConfigParse {
 public:
+
 	void parse();
-	void parseError(const parseException &ex) const;
+	void reportError(const ParseException &ex) const;
 	void openConfig();
 	void divideIntoServers(const std::string &line);
 	void parseServers();
 	void removeComments(std::string &line);
-	
+	void cutLocations();
+
 	size_t skipWhitespace(const std::string &line, size_t pos) const;
 	size_t findPair(const std::string &line, size_t pos) const;
 private:
 	std::ifstream config;
-	std::vector<serverBlock> servers;
+	std::vector<ServerBlock> servers;
 	std::vector<std::string> serversTxt;
 };
