f01fc2eaa6a7fac3b75638b8cc7ff856c7da040a
olnytim <yearagotomorrow@gmail.com>
2024-04-05 20:51:56 +0400

    I've started to combine parsing and sockets

--- srcs/sockets/WebServer.cpp
+++ srcs/sockets/WebServer.cpp
@@ -37,6 +37,20 @@ void WebServer::onMessageReceived(int client, const char* msg, int length) {
 		buffer << f.rdbuf();
 		content = buffer.str();
 	}
+	if (fileHtml.find(".html") != std::string::npos) {
+        contentType = "html";
+    } else if (fileHtml.find(".css") != std::string::npos) {
+        contentType = "css";
+    } else if (fileHtml.find(".jpeg") != std::string::npos) {
+        contentType = "jpeg";
+        std::fstream f(fileHtml.c_str(), std::ios::in | std::ios::binary);
+        if (f.good()) {
+            f.seekg(0, std::ios::end);
+            content.resize(f.tellg());
+            f.seekg(0, std::ios::beg);
+            f.read(&content[0], content.size());
+        }
+    }
 	f.close();
 	std::ostringstream response;
 	std::cout << content.size() << std::endl;
