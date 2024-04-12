956467c15516ecdbe7caf22d3da77e903f92dd1e
Ashot <ashotd03@gmail.com>
2024-04-12 18:21:52 +0400

    idk

--- includes/request/Request.hpp
+++ includes/request/Request.hpp
@@ -2,15 +2,22 @@
 
 #include <sstream>
 #include <iostream>
+#include <map>
 
 class Request{
 private:
 	std::string method;
 	std::string requestTarget;
-
+	std::string version;
+	std::map<std::string, std::string> headersMap;
 public:
 	const std::string &getMethod() const;
 	const std::string &getRequestTarget() const;
+	const std::string &getVersion() const;
+	const std::string &getHeadersMap() const;
+
 	void setMethod(const std::string &str);
 	void setRequestTarget(const std::string &str);
+	void setVersion(const std::string &version);
+	void setHeadersMap(const std::map<std::string, std::string> &headersMap);
 };
--- includes/request/RequestParsing.hpp
+++ includes/request/RequestParsing.hpp
@@ -9,6 +9,9 @@ private:
 public:
 	RequestParsing(Request &request);
 	void parseRequestLine(const std::string &reqLine);
+	void parseBody(const std::string &body);
+	void parseHeaders(const std::string &headers);
+
 	void parse(const std::string &request);
 	void method(std::stringstream &req);
 	void requestTarget(std::stringstream &req);
