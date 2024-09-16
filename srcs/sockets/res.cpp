aa040f90873f4a99328fad4d5b48c25fd7646495
lo <vladim1r.gribkov@yandex.ru>
2024-09-16 20:31:24 +0400

    refactor

--- srcs/sockets/res.cpp
+++ srcs/sockets/res.cpp
@@ -56,7 +56,7 @@ void Response::LocationMatch(const std::string& path, std::vector<LocationBlock>
 
 bool Response::isAllowedMethod(LocationBlock &location) {
     std::vector<std::string> methods = location.getMethods();
-    // Определяем строковое представление метода
+
     std::string method_str;
     printf("is allowedMethod Method: %d\n", request.method);
     switch (request.method) {
@@ -117,58 +117,73 @@ bool fileExists(std::string &file) {
 static std::string combinePaths(std::string p1, std::string p2, std::string p3)
 {
     std::string res;
-    int         len1;
-    int         len2;
+    int len1;
+    int len2;
 
     len1 = p1.length();
     len2 = p2.length();
-    if (p1[len1 - 1] == '/' && (!p2.empty() && p2[0] == '/') )
+
+    if (!p1.empty() && p1[len1 - 1] == '/' && (!p2.empty() && p2[0] == '/')) {
         p2.erase(0, 1);
-    if (p1[len1 - 1] != '/' && (!p2.empty() && p2[0] != '/'))
-        p1.insert(p1.end(), '/');
-    if (p2[len2 - 1] == '/' && (!p3.empty() && p3[0] == '/') )
+    }
+    if (!p1.empty() && p1[len1 - 1] != '/' && (!p2.empty() && p2[0] != '/')) {
+        p1 += "/";
+    }
+    if (!p2.empty() && p2[len2 - 1] == '/' && (!p3.empty() && p3[0] == '/')) {
         p3.erase(0, 1);
-    if (p2[len2 - 1] != '/' && (!p3.empty() && p3[0] != '/'))
-        p2.insert(p1.end(), '/');
+    }
+    if (!p2.empty() && p2[len2 - 1] != '/' && (!p3.empty() && p3[0] != '/')) {
+        p2 += "/";
+    }
+
     res = p1 + p2 + p3;
-    return (res);
+    return res;
 }
+
 
 
 
 bool Response::handleTarget() {
     std::string key;
     LocationMatch(request.path, server.getLocations(), key);
+
     if (!key.empty()) {
         LocationBlock loca = *server.getLocationKey(key);
-        if (isAllowedMethod(loca))
+
+        if (isAllowedMethod(loca)) {
             return true;
+        }
+
         if (request.body.length() > loca.getClientMaxBodySize()) {
             code = 413;
             return true;
         }
+
         if (checkReturn(loca, code, location)) {
             return true;
         }
 
-        if (!loca.getAlias().empty())
+        if (!loca.getAlias().empty()) {
             file = combinePaths(loca.getAlias(), request.path.substr(loca.getPath().length()), "");
-        else
+        } else {
             file = combinePaths(server.getRoot(), request.path, "");
+        }
+
         if (isDirectory(file)) {
             if (file[file.length() - 1] != '/') {
                 code = 301;
                 location = request.path + "/";
                 return true;
             }
+
             if (!loca.getIndex().empty()) {
                 file += loca.getIndex();
                 printf("File: '%s'\n", file.c_str());
-            }
-            else {
+            } else {
                 file += server.getIndex();
                 printf("File: '%s'\n", file.c_str());
             }
+
             if (!fileExists(file)) {
                 if (loca.getAutoindex()) {
                     auto_index = true;
@@ -178,47 +193,61 @@ bool Response::handleTarget() {
                 code = 404;
                 return true;
             }
+
             if (isDirectory(file)) {
                 code = 301;
-                if (!loca.getIndex().empty())
+                if (!loca.getIndex().empty()) {
                     location = combinePaths(request.path, loca.getIndex(), "");
-                else
+                } else {
                     location = combinePaths(request.path, server.getIndex(), "");
-                if (location[location.length() - 1] != '/')
+                }
+
+                if (location[location.length() - 1] != '/') {
                     location += "/";
+                }
+
                 return true;
             }
         }
-    }
-    else {
+    } else {
         file = combinePaths(server.getRoot(), request.path, "");
+
         if (isDirectory(file)) {
             printf("File length: %lu\n", file.length());
+
             if (file[file.length() - 1] != '/') {
                 code = 301;
                 location = request.path + "/";
                 return true;
             }
-            file += server.getIndex(); // why does getIndex return nothing?
+
+            file += server.getIndex();
             file += "index.html";
             printf("File: %s\n", file.c_str());
+
             if (!fileExists(file)) {
                 code = 403;
                 printf("File not found\n");
                 return true;
             }
+
             if (isDirectory(file)) {
                 code = 301;
                 location = combinePaths(request.path, server.getIndex(), "");
-                if (location[location.length() - 1] != '/')
+
+                if (location[location.length() - 1] != '/') {
                     location += "/";
+                }
+
                 return true;
             }
         }
     }
+
     printf("                                       h i                            \n");
     return false;
 }
+
 
 bool Response::readFile() {
     std::ifstream temp(file.c_str());
